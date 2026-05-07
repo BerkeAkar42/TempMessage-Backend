d167f2ff78451baec19b8aef7070a9e60c723704
Berke Akar <berkeakar4234@gmail.com>
2026-05-07 22:59:12 +0300

    feat: Message entity's migration has been updated

---
+++ API/Migrations/20260507195644_UpdateMessageEntity.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateMessageEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsEdited",
+                table: "Messages",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdateDate",
+                table: "Messages",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsEdited",
+                table: "Messages");
+
+            migrationBuilder.DropColumn(
+                name: "UpdateDate",
+                table: "Messages");
+        }
+    }
+}
